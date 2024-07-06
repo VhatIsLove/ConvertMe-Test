2c258fba83e19eb6c5b1204e3f7cd49a1d69080f
Vladislav Konovalov <vladislav.konovalov1@yandex.ru>
2024-07-06 20:28:40 +0300

    feat: adaptive header and offer block

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,32 @@
-const element = document.querySelector('.sey');
-const btn = document.querySelector('.btn');
+const burgerToggle = document.querySelector('.nav-toggle');
+const navigation = document.querySelector('.navigation');
+
+
+function openBurgerMenu() {
+  navigation.classList.add('open');
+  burgerButton.classList.add('navigation__active');
+}
+
+// Функция закрытия бургер меню
+function closeBurgerMenu() {
+  navigation.classList.remove('open');
+  burgerButton.classList.remove('navigation__active');
+}
+
+
+
+burgerToggle.addEventListener('click', () => {
+	if (burgerToggle.classList.contains('open')) {
+    closeBurgerMenu();
+  } else {
+    openBurgerMenu();
+  }
+
+})
+
+
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('.navigation') && !event.target.closest('.nav-toggle')) {
+    closeBurgerMenu();
+  }
+});
